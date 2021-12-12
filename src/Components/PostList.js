61d0e3b782eb74cc4dcfa4fc0fb80b33362b1da7
100rabSingh <100rab2000@gmail.com>
2021-12-12 17:56:39 +0530

    GET HTTP

---
+++ src/Components/PostList.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class PostList extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            post: [],
+            errorMessage: ''
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts1')
+            .then(response => {
+                console.log(response)
+                this.setState({ post: response.data })
+            })
+            .catch(error => {
+                console.log(error)
+                this.setState({ errorMessage: 'Error Retreiving Data' })
+            })
+    }
+
+    render() {
+        const { post, errorMessage } = this.state
+        return (
+            <div>
+                List Of Posts
+
+                {
+                    post.length ?
+                        post.map(post => <div key={post.id}>{post.title}</div>) :
+                        null
+                }
+                {
+                    errorMessage ? <div>{errorMessage}</div> : null
+                }
+            </div>
+        )
+    }
+}
+
+export default PostList
